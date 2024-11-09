477b4e5f2f1798c915cedd789077dcb75934899d
Christian <cgl_@hotmail.com>
2024-11-08 21:13:23 -0300

    Se Corrige Roles y se adiciona permisos en Layout de inicio.

--- Controllers/ActivitiesController.cs
+++ Controllers/ActivitiesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -41,6 +42,8 @@ namespace Sportify_Back.Controllers
 
             return View(activities);
         }
+
+        [Authorize(Policy = "AdministradorOnly")]
 
         // GET: Activities/Create
         public IActionResult Create()
